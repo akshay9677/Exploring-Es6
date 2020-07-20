0c30b043a767ff8c374a571a70b7c3620c73992e
Akshay Kannan <akshaykannan9677@gmail.com>
2020-07-20 13:52:23 +0530

    Chapter 10

--- Chapter13.test.js
+++ Chapter13.test.js
@@ -5,13 +5,13 @@ describe("Arrow functions in ES6", () => {
     var a = "Hello World";
     expect(obj.arrowFunction(a)).toBe(a);
   });
-  test("If Arrow function is used this keyword is not shadowed in nested functions", () => {
+  test("If Arrow function is used, this keyword is not shadowed in nested functions", () => {
     expect(obj.thisisNotShadowed.method()).toBe("Akshay");
   });
   test("Immediately Invoked Arrow Functions", () => {
     expect(obj.IIAF("Hello World")).toBe("Hello World");
   });
-  test("Arrow function bind loosley it loses conflict other operators", () => {
+  test("Arrow function bind loosley it loses conflict with other operators", () => {
     expect(obj.arrowFunctionLooselyBound(22)).toBe(22);
   });
   test("Returning Object literals", () => {
